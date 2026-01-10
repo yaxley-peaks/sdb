c2ccae2b423f3a81fcec56ad606b5927139cf2b9
yaxley-peaks <yax@yaxley.in>
2026-01-11 00:12:42 +0530

    readline loop

--- tools/sdb.cpp
+++ tools/sdb.cpp
@@ -6,10 +6,15 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string>
+
+#include <editline/readline.h>
 
 
 namespace {
-    auto attach(int argc, const char** argv) -> pid_t {
+    auto attach(int argc, const char **argv) -> pid_t {
         pid_t pid = 0;
         if (argc == 3 && argv[1] == std::string_view("-p")) {
             pid = static_cast<pid_t>(std::strtol(argv[2], nullptr, 0));
@@ -22,31 +27,61 @@ namespace {
                 return -1;
             }
         } else {
-        const char* program_path = argv[1];
+            const char *program_path = argv[1];
             if ((pid = fork()) < 0) {
                 std::perror("Fork failed");
                 return -1;
             }
-            if (pid == 0) { // In child
+            if (pid == 0) {
+                // In child
                 if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
                     std::perror("Tracing failed");
                     return -1;
                 }
-                if (execlp(program_path, program_path, nullptr) <0) {
+                if (execlp(program_path, program_path, nullptr) < 0) {
                     std::perror("Exec failed");
                     return -1;
                 }
             }
         }
-        return  pid;
+        return pid;
+    }
+
+    auto handle_command(pid_t pid, std::string_view line) -> void {
+
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, const char **argv) {
     if (argc == 1) {
         std::cerr << "No arguments given\n";
         return -1;
     }
 
-    pid_t pid = attach(argc, argv);
+    const pid_t pid = attach(argc, argv);
+
+    int wait_status = 0;
+    int options = 0;
+    if (waitpid(pid, &wait_status, options) < 0) {
+        std::perror("waitpid failed");
+    }
+
+    char *line = nullptr;
+
+    while ((line = readline("sdb> ")) != nullptr) {
+        std::string line_str;
+        if (line == std::string_view("")) {
+            free(line);
+            if (history_length > 0) {
+                line_str = history_list()[history_length - 1]->line;
+            }
+        } else {
+            line_str = line;
+            add_history(line);
+            free(line);
+        }
+        if (!line_str.empty()) {
+            handle_command(pid,line_str);
+        }
+    }
 }
